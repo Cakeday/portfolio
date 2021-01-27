d2dfc82f77bd382b3bcccfd2b376b51fe9c999da
Caleb Reyes <sciencecamp123@gmail.com>
2021-01-26 17:08:35 -0800

    Updates

---
+++ precache-manifest.e4a773d53ff6d16d87812a4afb53b7e7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1be957294358281ea9bb178e53b2a1e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9c3397073a12340dbe4e",
+    "url": "/portfolio/static/js/2.ee34472b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.ee34472b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d770daa57c7693d7f80",
+    "url": "/portfolio/static/js/main.9b002de8.chunk.js"
+  },
+  {
+    "revision": "9932c21051d16330cc85",
+    "url": "/portfolio/static/js/runtime-main.2299ec0b.js"
+  },
+  {
+    "revision": "72363dbb33b4b68f660847c883da3796",
+    "url": "/portfolio/static/media/PortlandMe.72363dbb.JPG"
+  },
+  {
+    "revision": "2582703935d28a46fb87721c9cd2fda5",
+    "url": "/portfolio/static/media/WhatDoYouMern1.25827039.png"
+  },
+  {
+    "revision": "2d1da261b112401420917a4742aeac59",
+    "url": "/portfolio/static/media/justonepenny1.2d1da261.png"
+  },
+  {
+    "revision": "af98075bedea7d3eef7ebf0dc2064149",
+    "url": "/portfolio/static/media/unibui1.af98075b.png"
+  }
+]);
