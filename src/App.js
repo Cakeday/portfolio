c24f3f1211aa7cc260dd447f485b79f01e21090c
Caleb Reyes <sciencecamp123@gmail.com>
2021-02-17 00:20:59 -0800

    removed react-ga

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import Hero from './components/Hero';
 import About from './components/About'
 import WorkAndProjects from './components/WorkAndProjects'
 
-import ReactGA from 'react-ga';
-
 const App = () => {
 
   const scrollRef = useRef()
@@ -14,9 +12,6 @@ const App = () => {
     scrollRef.current.scrollIntoView({behavior: "smooth"})
   }
 
-  const trackingId = "G-9XLHX3WTGK"; // Replace with your Google Analytics tracking ID
-  ReactGA.initialize(trackingId);
-  ReactGA.pageview(window.location.pathname);
 
 
   return (
